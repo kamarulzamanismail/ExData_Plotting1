6e1c69a413d3666b35f5738e1f862fc919b11807
kamarulzamanismail <kamarulzaman.ismail@moe.gov.my>
2017-06-14 09:10:59 +0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,1 @@
+plot(house_sub$dt, house_sub$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
---
+++ plot3.R
@@ -0,0 +1,4 @@
+plot(house_sub$dt, house_sub$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+points(house_sub$dt, house_sub$Sub_metering_2, type = "l", col = "red")
+points(house_sub$dt, house_sub$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
