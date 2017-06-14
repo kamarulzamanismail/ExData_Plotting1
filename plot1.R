6948571850077f0665e0a30aa8ab7ba659155c8f
kamarulzamanismail <kamarulzaman.ismail@moe.gov.my>
2017-06-14 10:24:25 +0800

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -1,1 +1,3 @@
 hist(house_sub$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", breaks = 13, ylim = c(0,1200), xlim = c(0, 6))
+dev.copy(png, 'plot1.png')
+dev.off()
