d11ccb429432aea7f9370797b0b98a0e34c77ef4
kamarulzamanismail <kamarulzaman.ismail@moe.gov.my>
2017-06-14 09:14:39 +0800

    Delete LoadData_house.R

--- LoadData_house.R
+++
@@ -1,4 +0,0 @@
-house <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
-> house$dt <- strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S")
-> house$Date <- as.Date(hpc$Date, "%d/%m/%Y")
-> house_sub <- subset(hpc, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
